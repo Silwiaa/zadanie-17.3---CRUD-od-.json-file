60e34286fb9d9f6d6beb6dcc084668955cf9e57e
Michael Gacek <gacek.michael@outlook.com>
2018-07-26 20:12:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ var express = require('express'),
 app.use(bodyParser.json());
 
 app.get('/getNote', function(req, res) {
-    fs.readFile('./test,json', 'utf-8', function(err, data) {
+    fs.readFile('./test.json', 'utf-8', function(err, data) {
         if (err) throw err;
         stringifyFile = data;
         res.send(data);
@@ -16,11 +16,17 @@ app.get('/getNote', function(req, res) {
 });
     
 app.post('/updateNote/:note', function(req, res) {
-    fs.writeFile('./test.json', stringifyFile, function(err) {
-        if(err) throw err;
-        console.log('file updated');
-        res.send(stringifyfile);
-    });
+    fs.readFile('./test.json', 'utf-8', function(err, data) {
+        if (err) throw err;
+
+        data += req.params.note;
+
+        fs.writeFile('./test.json', data, function(err) {
+            if(err) throw err;
+            console.log('file updated');
+            res.send(data);
+        });
+    });    
 });
 
 app.listen(3000);
